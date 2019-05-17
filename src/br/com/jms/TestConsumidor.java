5bacb8dc8c3f61f048aa37baa4f5674b6dd6e974
rgomsilv <rgomsilv@SAO-D1GW9R2.usersad.everis.int>
2019-05-17 09:28:55 -0300

    novo pojeto

---
+++ src/br/com/jms/TestConsumidor.java
@@ -0,0 +1,13 @@
+package br.com.jms;
+
+import javax.jms.Connection;
+
+public class TestConsumidor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//	Connection = factory
+	}
+
+}
